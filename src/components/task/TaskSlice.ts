300e83f16c73bd98caf0d6606c24b00697bb61aa
Jesús Cobacho Aguilera <jcobacho90@gmail.com>
2024-11-03 08:55:53 -0500

    [NEW] create task slice

---
+++ src/components/task/TaskSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export type TaskType = {
+  id: number;
+  completed: boolean;
+  title: string;
+};
+
+export const taskApi = createApi({
+  reducerPath: "taskApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `https://jsonplaceholder.typicode.com/`
+  }),
+
+  endpoints: builder => ({
+    getAllTasks: builder.query<TaskType[], void>({
+      query: () => ({
+        url: "todos/?userId=1",
+      })
+    })
+  })
+});
+
+const taskSlice = createSlice({
+  name: "task",
+  initialState: [] as TaskType[],
+  reducers: {
+    addTodo: (state, { payload }) => {
+      return [payload, ...state];
+    },
+    deleteTodo: (state, { payload }) => {
+      sessionStorage.setItem("isAuthenticated", "true");
+      sessionStorage.setItem("user", `${JSON.stringify(state)}`);
+      return state;
+    }
+  }
+});
+
+
+export default taskSlice.reducer;
+export const { addTodo, deleteTodo,  } = taskSlice.actions;
+
+// Exporting the generated methods from createApi
+export const {
+	useGetAllTasksQuery,
+} = taskApi;
