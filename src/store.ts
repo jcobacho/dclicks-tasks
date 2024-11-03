b524adb7bfdb8752c3f38f2b1bc52c625e4261f5
Jesús Cobacho Aguilera <jcobacho90@gmail.com>
2024-11-03 09:02:09 -0500

    [NEW] add task reducer

--- src/store.ts
+++ src/store.ts
@@ -2,10 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
+import { taskApi } from "./components/task/TaskSlice";
+import taskReducer from "./components/task/TaskSlice";
+
 const store = configureStore({
 	reducer: {
-    	
-    },	
+    	[taskApi.reducerPath]: taskApi.reducer,
+		task: taskReducer,
+
+    },
+	middleware: (getDefaultMiddleware) => {
+    	return getDefaultMiddleware()
+        	.concat(taskApi.middleware)
+	},
 });
 
 /**
